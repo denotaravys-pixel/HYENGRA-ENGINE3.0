5e12371dbed5be791b22b667f018e4ce40f79f3e
denotaravys-pixel <denotaravys@gmail.com>
2026-04-17 14:19:01 +0200

    Register chunk rendering event before world render

--- src/client/java/com/hyengra/mod/HyengaModClient.java
+++ src/client/java/com/hyengra/mod/HyengaModClient.java
@@ -2,10 +2,18 @@ package com.hyengra.mod;
 
 import com.hyengra.mod.render.ChunkRenderer;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 
 public class HyengaModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         ChunkRenderer.init();
+
+        WorldRenderEvents.BEFORE_CHUNKS.register(context -> {
+            ChunkRenderer.onBeforeWorldRender(
+                context.camera(),
+                context.tickCounter()
+            );
+        });
     }
 }
